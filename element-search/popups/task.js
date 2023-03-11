ba0c47c49b94d4007de11d3ba5e469810810e26e
JulianaAstra <julianaas@yandex.ru>
2023-03-11 14:18:25 +0300

    решение задачи

---
+++ element-search/popups/task.js
@@ -0,0 +1,18 @@
+const popupMain = document.querySelector('#modal_main');
+const popupSuccess = document.querySelector('#modal_success');
+const close = document.querySelectorAll('.modal__close_times');
+
+popupMain.classList.add('modal_active');
+popupMain.querySelector('.show-success').addEventListener('click', () => {
+    popupSuccess.classList.add('modal_active');
+});
+
+close.forEach(element => {
+    element.addEventListener('click', () => {
+        popupSuccess.classList.remove('modal_active');
+        popupMain.classList.remove('modal_active');
+    });
+});
+
+
+
